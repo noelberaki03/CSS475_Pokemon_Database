96fd52e6bc255e92f6c8b3107d67a60455fd26de
Noel Beraki <noel30@uw.edu>
2024-05-29 12:11:34 -0700

    Example Project

---
+++ css475php_example/config.inc.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MKochanski
+ * Date: 7/24/2018
+ * Time: 2:47 PM
+ */
+$servername = 'noel30.vergil.u.washington.edu'; // Your Server (if noot)
+$username = 'root';
+$password = 'CSS475$';
+$database = 'point_of_sale';
+$port = 3475; // Your Port
